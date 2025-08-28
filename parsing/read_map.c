023270ac8c6d190088c826261dbc19ad021cf4e8
Mohammed Akarkaou <makarkao@c1r2p3.1337.ma>
2025-08-28 16:35:18 +0100

    cub3d-improved-v11

--- parsing/read_map.c
+++ parsing/read_map.c
@@ -6,7 +6,7 @@
 /*   By: makarkao <makarkao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 18:20:19 by makarkao          #+#    #+#             */
-/*   Updated: 2025/08/26 18:26:40 by makarkao         ###   ########.fr       */
+/*   Updated: 2025/08/28 16:34:11 by makarkao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 void file_to_image(t_game *game, char **sp_line, t_texture *tex)
 {
 	tex->texture_p = mlx_xpm_file_to_image(game->mlx, sp_line[1], &tex->texture_w, &tex->texture_h);
-	if(!tex->texture_p)
+	if (!tex->texture_p)
 		exit((free_strs(sp_line), printf("ERROR\n"), 1));
 	tex->texture_s = (unsigned int *)mlx_get_data_addr(tex->texture_p, &tex->bpp, &tex->texture_len, &tex->endian);
-	if(!tex->texture_s)
+	if (!tex->texture_s)
 		exit((free_strs(sp_line), printf("ERROR\n"), 1));
 }
 
 void fill_texture(t_game *game, char **sp_line, int flag)
 {
-	if(flag == EA)
+	if (flag == EA)
 		file_to_image(game, sp_line, &game->cub->tex_ea);
-	else if(flag == WE)
+	else if (flag == WE)
 		file_to_image(game, sp_line, &game->cub->tex_we);
-	else if(flag == SO)
+	else if (flag == SO)
 		file_to_image(game, sp_line, &game->cub->tex_so);
-	else if(flag == NO)
+	else if (flag == NO)
 		file_to_image(game, sp_line, &game->cub->tex_no);
 }
 
 void fill_texture_tt(t_game *game, char **sp_line, int flag)
 {
-	if(game->cub->state_mask & flag)
+	if (game->cub->state_mask & flag)
 		exit((printf("ERROR\n"), 1));
 	else
 	{
@@ -45,9 +45,56 @@ void fill_texture_tt(t_game *game, char **sp_line, int flag)
 	}
 }
 
+void extract_color(t_game *game, char **sp_line, int flag)
+{
+	int i;
+	int j;
+	int comma_flag;
+	int number;
+	int count;
+	int *rgb;
+
+	i = 1;
+	count = 0;
+	number = 0;
+	rgb = (int[3]){-1, -1, -1};
+	comma_flag = 1;
+	while (sp_line[i])
+	{
+		j = 0;
+		while (sp_line[i][j])
+		{
+			if (check_numeric(sp_line[i][j]))
+			{
+				number += (sp_line[i][j] - 48);
+				if (number > 255)
+					exit((printf("ERROR\n"), 1));
+				if (comma_flag)
+					comma_flag = 0;
+			}
+			else if (sp_line[i][j] == ',' && !comma_flag)
+			{
+				count++;
+				if (count > 2)
+					exit((printf("ERROR\n"), 1));
+				comma_flag = 1;
+			}
+			else
+				exit((printf("ERROR\n"), 1));
+			rgb[i - 1] = number;
+			j++;
+		}
+		i++;
+	}
+	if(flag == F)
+		game->cub->color_f = (t_rgb){rgb[0], rgb[1], rgb[2]};
+	else
+		game->cub->color_c = (t_rgb){rgb[0], rgb[1], rgb[2]};
+}
+
 void check_line(t_game *game, char **sp_line, char *line)
 {
-	if(!ft_strcmp(sp_line[0], "EA"))
+	if (!ft_strcmp(sp_line[0], "EA"))
 		fill_texture_tt(game, sp_line, EA);
 	else if (!ft_strcmp(sp_line[0], "SO"))
 		fill_texture_tt(game, sp_line, SO);
@@ -63,27 +110,25 @@ void check_line(t_game *game, char **sp_line, char *line)
 
 int ft_check(char *str)
 {
-	return (!ft_strcmp(str, "NO") || !ft_strcmp(str, "SO") 
-		|| !ft_strcmp(str, "WE") || !ft_strcmp(str, "EA") 
-		|| !ft_strcmp(str, "F") || !ft_strcmp(str, "C"));
+	return (!ft_strcmp(str, "NO") || !ft_strcmp(str, "SO") || !ft_strcmp(str, "WE") || !ft_strcmp(str, "EA") || !ft_strcmp(str, "F") || !ft_strcmp(str, "C"));
 }
 
-char**	read_map(t_game *game, t_cub_lines *cub_lines_list)
+char **read_map(t_game *game, t_cub_lines *cub_lines_list)
 {
-	char	**sp_line = NULL;
+	char **sp_line = NULL;
 	char *line;
 
 	while (cub_lines_list)
 	{
 		line = cub_lines_list->line;
 		sp_line = ft_split(cub_lines_list->line, ' ');
-		if(!sp_line)
-			return(printf("Error\n"), NULL);
-		if(sp_line[0] && (!sp_line[1] || (sp_line[1] && sp_line[2])))
-			return(printf("Error\n"), NULL);
-		if(sp_line[0][0])
-		{		
-			if(ft_check(sp_line[0]))
+		if (!sp_line)
+			return (printf("Error\n"), NULL);
+		if (sp_line[0] && (!sp_line[1] || (sp_line[1] && sp_line[2])))
+			return (printf("Error\n"), NULL);
+		if (sp_line[0][0])
+		{
+			if (ft_check(sp_line[0]))
 				check_line(game, sp_line, line);
 			else
 				break;
@@ -91,9 +136,9 @@ char**	read_map(t_game *game, t_cub_lines *cub_lines_list)
 		(free_strs(sp_line), sp_line = NULL);
 		cub_lines_list = cub_lines_list->next;
 	}
-	if(sp_line)
+	if (sp_line)
 		(free_strs(sp_line), sp_line = NULL);
-	if(game->cub->state_mask != (NO | SO | EA | WE | F | C))
+	if (game->cub->state_mask != (NO | SO | EA | WE | F | C))
 		exit((printf("ERROR\n"), 1));
 	// fill_map(game);
 	printf("nadi\n");
--- ttt.c
+++ ttt.c
@@ -1,34 +1,54 @@
 #include "cub3d.h"
 
 
-int mouse_hook(int button, int x, int y, void *param)
-{
-    if (button == 1) // left click
-        printf("Left click at (%d, %d)\n", x, y);
-    else if (button == 3) // left click
-        printf("right click at (%d, %d)\n", x, y);
-    else if (button == 2) // left click
-        printf("middle click at (%d, %d)\n", x, y);
-    else if (button == 4) // scroll up
-        printf("Scroll up at (%d, %d)\n", x, y);
-    else if (button == 5) // scroll up
-        printf("Scroll down at (%d, %d)\n", x, y);
-    else if (button == 6) // scroll left
-        printf("Scroll left\n");
-    else if (button == 7) // scroll right
-        printf("Scroll right\n");
+// int mouse_hook(int button, int x, int y, void *param)
+// {
+//     if (button == 1) // left click
+//         printf("Left click at (%d, %d)\n", x, y);
+//     else if (button == 3) // left click
+//         printf("right click at (%d, %d)\n", x, y);
+//     else if (button == 2) // left click
+//         printf("middle click at (%d, %d)\n", x, y);
+//     else if (button == 4) // scroll up
+//         printf("Scroll up at (%d, %d)\n", x, y);
+//     else if (button == 5) // scroll up
+//         printf("Scroll down at (%d, %d)\n", x, y);
+//     else if (button == 6) // scroll left
+//         printf("Scroll left\n");
+//     else if (button == 7) // scroll right
+//         printf("Scroll right\n");
 
-    return (0);
+//     return (0);
+// }
+
+
+
+
+// typedef struct s_rgb
+// {
+//     int r;
+//     int g;
+//     int b;
+// } t_rgb;
+void assign(t_rgb *rgb)
+{
+    *rgb = (t_rgb){88, 99 , 77};
 }
 
 int main(void)
 {
-    void *mlx;
-    void *win;
+    t_rgb rgb = {10, 23, 57};
 
-    mlx = mlx_init();
-    win = mlx_new_window(mlx, 800, 600, "Mouse Hook Example");
-
-    mlx_mouse_hook(win, mouse_hook, NULL);
-    mlx_loop(mlx);
+    printf("before ->  r = %d, g = %d, b = %d  \n", rgb.r, rgb.r, rgb.b);
+    assign(&rgb);
+    printf("after   ->  r = %d, g = %d, b = %d  \n", rgb.r, rgb.r, rgb.b);
 }
+
+    // void *mlx;
+    // void *win;
+
+    // mlx = mlx_init();
+    // win = mlx_new_window(mlx, 800, 600, "Mouse Hook Example");
+
+    // mlx_mouse_hook(win, mouse_hook, NULL);
+    // mlx_loop(mlx);
