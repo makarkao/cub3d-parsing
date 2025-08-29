ebf921ffe7ed7edea00d39f782d6c9fba77c3faa
Mohammed Akarkaou <makarkao@c1r2p5.1337.ma>
2025-08-29 10:09:23 +0100

    cub3d-parsing-v13

--- parsing/check_map.c
+++ parsing/check_map.c
@@ -2,5 +2,19 @@
 
 void    check_map(char  **map)
 {
-    
+    int i;
+    int j;
+
+    i = 0;
+    while(map[i][j])
+    {
+        j = 0;
+        while(map[i][j])
+        {
+
+
+            j++;
+        }
+        i++;
+    }
 }
